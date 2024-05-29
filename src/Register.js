47d4b810a7c2220bbc3d9e2c1f9b4a3e90775076
백대홍 <bdh3620@naver.com>
2024-05-29 23:29:51 +0900

    Feat: ToolTip(ver.1)

--- src/Register.js
+++ src/Register.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Box, TextField, Button, FormControlLabel, Radio, RadioGroup, Container, Typography, Paper } from '@mui/material';
-import 'react-tooltip/dist/react-tooltip.css'
-import { Tooltip } from 'react-tooltip'
+import 'react-tooltip/dist/react-tooltip.css';
+import { Tooltip } from 'react-tooltip';
 
 const Register = () => {
   const [UserId, setUserId] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [location, setLocation] = useState('A');
+  const [location, setLocation] = useState('AndN');
   const navigate = useNavigate();
 
   const handleRegister = () => {
@@ -65,9 +65,30 @@ const Register = () => {
             row
             sx={{ justifyContent: 'center', marginTop: 2 }}
           >
-            <FormControlLabel value="AndN" control={<Radio />} label="앤드앤" />
-            <FormControlLabel value="Client" control={<Radio />} label="클라이언트" />
-            <FormControlLabel value="Outsourcing" control={<Radio />} label="외주업체" />
+            <div>
+              <div data-tooltip-id="AndN">
+                <FormControlLabel value="AndN" control={<Radio />} label="앤드앤" />
+              </div>
+              <Tooltip id="AndN" place="top" effect="solid">
+                앤드앤 회사에 속한 경우 선택하세요
+              </Tooltip>
+            </div>
+            <div>
+              <div data-tooltip-id="Client">
+                <FormControlLabel value="Client" control={<Radio />} label="클라이언트" />
+              </div>
+              <Tooltip id="Client" place="top" effect="solid">
+                클라이언트인 경우 선택하세요
+              </Tooltip>
+            </div>
+            <div>
+              <div data-tooltip-id="Outsourcing">
+                <FormControlLabel value="Outsourcing" control={<Radio />} label="외주업체" />
+              </div>
+              <Tooltip id="Outsourcing" place="top" effect="solid">
+                외주업체인 경우 선택하세요
+              </Tooltip>
+            </div>
           </RadioGroup>
           <Button
             fullWidth
@@ -85,6 +106,7 @@ const Register = () => {
 };
 
 export default Register;
+
 
 
 //i 버튼 추가해서 카테고리중에서 어떤걸 선택해야 하는지 알려주는 tip을 적어본다면 어떤가?
