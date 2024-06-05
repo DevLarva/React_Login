9aa7aaaa00d176e29182aab81a299db757922619
백대홍 <bdh3620@naver.com>
2024-06-05 22:48:49 +0900

    Feat: 로그인 페이지 UI 수정

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import GlobalContext from './GlobalContext';
 import { Box, TextField, Button, FormControlLabel, Radio, RadioGroup, Container, Typography, Paper, Grid } from '@mui/material';
+import Andnlogo from './assets/andnlogo.png'
 
 const Login = () => {
   const { setUser } = useContext(GlobalContext);
@@ -17,10 +18,16 @@ const Login = () => {
 
   return (
     <Container component="main" maxWidth="xs">
-      <Paper elevation={3} sx={{ padding: 2, marginTop: 8 }}>
-        <Typography component="h1" variant="h5" align="center">
-          Login
-        </Typography>
+      <Typography component="h2" variant="h1" align="center" sx={{ marginTop: 4 }} >
+        <img
+          src={Andnlogo}
+          alt="And N"
+          height={70}
+          style={{ filter: 'invert(1)' }}
+        />
+      </Typography>
+      <Paper elevation={5} sx={{ padding: 2, marginTop: 2 }}>
+
         <Box component="form" noValidate sx={{ mt: 1 }}>
           <TextField
             margin="normal"
@@ -53,31 +60,32 @@ const Login = () => {
             로그인
           </Button>
 
-          <Grid container spacing={2} justifyContent="space-between">
-            <Grid item xs={4}>
-              <Link to="/find-password" style={{ textDecoration: 'none' }}>
-                <Button fullWidth color="primary">
-                  비밀번호 찾기
-                </Button>
-              </Link>
-            </Grid>
-            <Grid item xs={4}>
-              <Link to="/find-userid" style={{ textDecoration: 'none' }}>
-                <Button fullWidth color="primary">
-                  아이디 찾기
-                </Button>
-              </Link>
-            </Grid>
-            <Grid item xs={4}>
-              <Link to="/register" style={{ textDecoration: 'none' }}>
-                <Button fullWidth color="primary">
-                  회원가입
-                </Button>
-              </Link>
-            </Grid>
-          </Grid>
+
         </Box>
       </Paper>
+      <Grid container spacing={1} justifyContent="space-between" sx={{ marginTop: 0.1 }}>
+        <Grid item xs={4}>
+          <Link to="/find-password" style={{ textDecoration: 'none' }} fullWidth color='primary'>
+            <Button fullWidth color="primary">
+              비밀번호 찾기
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={4}>
+          <Link to="/find-userid" style={{ textDecoration: 'none' }}>
+            <Button fullWidth color="primary">
+              아이디 찾기
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={4}>
+          <Link to="/register" style={{ textDecoration: 'none' }}>
+            <Button fullWidth color="primary">
+              회원가입
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
